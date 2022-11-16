5531b9fef76307999a3cbd6c93096881d8480669
Huy Trinh <huy.trinh20102001@gmail.com>
2022-11-16 11:38:47 +0700

    init lab 2

---
+++ Core/Inc/led_display.h
@@ -0,0 +1,15 @@
+/*
+ * led_display.h
+ *
+ *  Created on: Nov 12, 2022
+ *      Author: trinh huy
+ */
+
+#ifndef INC_LED_DISPLAY_H_
+#define INC_LED_DISPLAY_H_
+
+
+void reset7SEG();
+void display7SEG(int);
+
+#endif /* INC_LED_DISPLAY_H_ */
